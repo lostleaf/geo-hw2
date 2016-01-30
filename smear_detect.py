02666d128b4043ad7665847f6a8af0c8e05aa0ef
lostleaf <lostleaf@me.com>
2016-01-29 20:13:57 -0600

    Extract code in ipython notebook into smear_detect.py

---
+++ smear_detect.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+def main():
+    file_names = glob.glob("*.jpg")
+    img = cv2.imread(file_names[0])
+    img_avg = np.zeros_like(img, dtype=np.float64)
+    for name in file_names:
+        img = cv2.imread(name)
+        img_avg += img
+    img_avg /= len(file_names)
+    plt.imshow(cv2.cvtColor(img_avg.astype(np.uint8), cv2.COLOR_BGR2RGB))
+    img_avg_gray = cv2.cvtColor(img_avg.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+    variance = np.zeros_like(img_avg_gray, dtype=np.float)
+    for name in file_names:
+        img = cv2.imread(name)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.float)
+        variance += (img_gray - img_avg_gray) ** 2
+    variance /= len(file_names)
+    plt.imshow(variance < 175, cmap="gray")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
