dec53b1194ae46aab6deebff798957cd4dc94366
lostleaf <lostleaf@me.com>
2016-01-29 22:49:04 -0600

    compute variance for cam_0 and cam_3

--- smear_detect.py
+++ smear_detect.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import glob
 
 def main():
-    file_names = glob.glob("*.jpg")
+    file_names = glob.glob("../cam_3/*.jpg")[:500]
     img = cv2.imread(file_names[0])
     img_avg = np.zeros_like(img, dtype=np.float64)
     for name in file_names:
@@ -19,7 +19,10 @@ def main():
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.float)
         variance += (img_gray - img_avg_gray) ** 2
     variance /= len(file_names)
-    plt.imshow(variance < 175, cmap="gray")
+    plt.imshow(variance, cmap="gray")
+    plt.colorbar()
+    plt.show()
+    plt.imshow(variance<500, cmap="gray")
     plt.show()
 
 if __name__ == '__main__':
